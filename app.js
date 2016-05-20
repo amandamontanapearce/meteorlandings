2cd07eaa7bcd9572f5c3fd55012f36b3378270c8
AmandaMontana Pearce <amandamontanapearce@gmail.com>
2016-05-20 12:51:20 -0600

    height not show

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
             google.maps.event.trigger(map, 'resize');
             $('.results').show();
             google.maps.event.trigger(map, 'resize'); ////
-            $('#map').show();////
+            $('#map').height(300);////
 
             geoJSON.features.push(
             {
